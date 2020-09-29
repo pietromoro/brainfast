027b24f2e2f3efeaed82a706ac5aee4005645738
Pietro Moro <pietromoro@programmer.net>
2020-09-29 15:56:11 +0200

    Tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn tokenize(input: &str) -> Vec<Token> {
+  let mut tokens = Vec::<Token>::new();
+  let mut chars = input.chars();
+
+  while let Some(c) = chars.next() {
+    match c {
+      '+' => tokens.push(Add),
+      '-' => tokens.push(Sub),
+      '<' => tokens.push(Right),
+      '>' => tokens.push(Left),
+      ',' => tokens.push(Read),
+      '.' => tokens.push(Write),
+      '[' => tokens.push(BeginLoop),
+      ']' => tokens.push(EndLoop),
+      _ => {}
+    }
+  }
+
+  tokens
+}
